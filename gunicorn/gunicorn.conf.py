94185bf0a0806c7446c65a7712dcb991e67bc1ce
mafro <github@mafro.net>
2014-05-17 12:31:21 +1000

    reworked gunicorn config with better defaults

--- gunicorn/gunicorn.conf.py
+++ gunicorn/gunicorn.conf.py
@@ -1,22 +1,31 @@
 # vim: set ft=jinja:
 #################################################
-# Gunicorn config for ogreserver
+# Gunicorn config for {{ app_name }}
 #################################################
 
 bind = '{{ gunicorn_host }}:{{ gunicorn_port }}'
+worker_class = '{{ worker_class }}'
+
+# configure number of gunicorn workers
 {% if 'env' in grains and grains['env'] == 'dev' %}
 workers = 1
+{% elif workers is defined %}
+workers = {{ workers }}
 {% else %}
 import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 {% endif %}
-#worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
-backlog = 2048
-worker_class = '{{ worker_class }}'
-debug = True
+
+# dont daemonize; use supervisor
 daemon = False
 timeout = {{ timeout }}
 proc_name = 'gunicorn-{{ app_name }}'
 pidfile = '/tmp/gunicorn-{{ app_name }}.pid'
+
+# error log to STDERR
 errorlog = '-'
+{% if 'env' in grains and grains['env'] == 'dev' %}
+loglevel = 'debug'
+{% else %}
 loglevel = '{{ loglevel }}'
+{% endif %}
