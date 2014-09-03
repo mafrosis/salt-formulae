c512d1aaf11476e7bb739e94895840d712cfb918
mafro <github@mafro.net>
2014-09-03 17:05:01 +1000

    tmux: tmux-powerline pid segment supports multiple pidfiles now

--- tmux/pid-segment.tmpl.sh
+++ tmux/pid-segment.tmpl.sh
@@ -1,14 +1,14 @@
 # print the current {{ component_name }} pid
 
 run_segment() {
-	# check pid file exists
-	if [ ! -f "/tmp/{{ component_name }}-{{ pillar['app_name'] }}.pid" ]; then
+	# check any pid files exist
+	if ! ls /tmp/{{ component_name }}-{{ pillar['app_name'] }}*.pid &> /dev/null; then
 		echo "DEAD"
 		return 0
 	fi
 
 	# print the current {{ component_name }} pid
-	pgrep -d "," -P $(cat /tmp/{{ component_name }}-{{ pillar['app_name'] }}.pid)
+	pgrep -d "," -P "$(cat /tmp/{{ component_name }}-{{ pillar['app_name'] }}*.pid | awk -vORS=, '{print}' | sed 's/,$/\n/')"
 
 	if [ $? == 1 ]; then
 		echo "DEAD"
